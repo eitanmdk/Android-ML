d910aa443bd4d4f600515af19b48d74530f25f91
EitanMdk <72807177+eitanmdk@users.noreply.github.com>
2021-08-17 17:46:11 +0000

    real commit

--- x32/jni/src/main.cpp
+++ x32/jni/src/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <cstdio>
 #include <cstdlib>
 #include <dlfcn.h>
@@ -11,45 +12,29 @@ namespace h_menuLayer {
 	void* (*o_init)(CCLayer*);
 	void* init(CCLayer* self) {
 		auto ret = o_init(self);
-		auto sprite = CCSprite::createWithSpriteFrameName("GJ_playBtn_001.png");
-		auto btn = CCMenuItemSprite::create(sprite, sprite, self, menu_selector(CreatorLayer::onDailyLevel));
-		btn->setScale(0.5);
+    
+    //create button
+
+		auto sprite = CCSprite::createWithSpriteFrameName("GJ_createBtn_001.png");
+
+		auto btn = CCMenuItemSprite::create(sprite, sprite, self, menu_selector(CreatorLayer::onMyLevels));
+		btn->setScale(0.7);
+
 		auto menu = CCMenu::create();
 		menu->addChild(btn, 100);
-		menu->setPosition({CCDirector::sharedDirector()->getVisibleSize().width / 2, 100});
-		self->addChild(menu, 100);
-		return ret;
-	}
-}
+		menu->setPosition({CCPoint(dir->getScreenRight() - 43, dir->getScreenBottom() + 43 });
 
-// https://github.com/FlairyDash/rgb-icons-mod/blob/main/main.cpp
-namespace h_playerObject {
-	CCNode* (*o_create)(CCNode*, int, int, CCLayer*);
-	CCNode* create(CCNode* self, int i1, int i2, CCLayer* layer) {
-		if(self != nullptr) {
-			CCNode* ret = o_create(self, i1, i2, layer);
-			ret->runAction(CCRepeatForever::create(CCSequence::create(
-				CCTintTo::create(0.5, 255,   0,	  0), 
-				CCTintTo::create(0.5, 255, 255,   0), 
-				CCTintTo::create(0.5, 0  , 255,   0),
-				CCTintTo::create(0.5, 0  , 255, 255), 
-				CCTintTo::create(0.5, 0  ,   0, 255), 
-				CCTintTo::create(0.5, 255,   0, 255), 
-				nullptr)));
-			CCNode* a = (CCNode*)ret;
-			CCNode* b = (CCNode*)a->getChildren()->objectAtIndex(0);
-			CCNode* c = (CCNode*)b->getChildren()->objectAtIndex(0);
-			c->runAction(CCRepeatForever::create(CCSequence::create(
-				CCTintTo::create(0.5, 0  , 255, 255), 
-				CCTintTo::create(0.5, 0  ,   0, 255), 
-				CCTintTo::create(0.5, 255,   0, 255), 
-				CCTintTo::create(0.5, 255,   0,	  0), 
-				CCTintTo::create(0.5, 255, 255,   0), 
-				CCTintTo::create(0.5, 0  , 255,   0),
-				nullptr)));
-			return ret;
-		}
-		return o_create(self, i1, i2, layer);
+		self->addChild(menu, 100);
+
+    //texto de la version uwu
+
+    auto texto = CCLabelMBFont::create("Version 2.2.0.3", "chatFont.fnt");
+
+    texto->setPosition(CCPoint(dir->getScreenLeft() + 2, dir->getScreenTop() - 10));
+
+    label->setScale(.5);
+
+		return ret;
 	}
 }
 
@@ -57,5 +42,5 @@ namespace h_playerObject {
 __attribute__((constructor))
 void fdml_init() {
 	hook("_ZN9MenuLayer4initEv", h_menuLayer, init, o_init);
-	hook("_ZN12PlayerObject6createEiiPN7cocos2d7CCLayerE", h_playerObject, create, o_create);	inlineHookAll();
 }
+*/
