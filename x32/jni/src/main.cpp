78f16e42929aabf3c69b61869ff627379038781e
iAndyHD3 <54410739+iAndyHD3@users.noreply.github.com>
2021-08-18 16:03:33 +0200

    ya
    ya

--- x32/jni/src/main.cpp
+++ x32/jni/src/main.cpp
@@ -7,47 +7,69 @@
 
 using namespace cocos2d;
 
-namespace h_MenuLayer {
+namespace h_menuLayer {
+
     
     void* (*o_init)(CCLayer*);
     void* init(CCLayer* self) {
         auto ret = o_init(self);
-        
-        auto gm = GameManager::sharedState();
-        
-        gm->setGameVariable("0115", false); // FPS label thing
-        gm->setGameVariable("0109", false); // level info thing
-        gm->setGameVariable( "0053", false ); // more games thing
-        
-        gm->setHasRatingPower( 1 ); // fix mod button thing
+		
+		
         
         auto dir = CCDirector::sharedDirector();
-
+		
         auto menu = CCMenu::create();
         auto menu2 = CCMenu::create();
+
         
         // Created Lvls
         auto createdlvls = CCSprite::createWithSpriteFrameName("GJ_createBtn_001.png");
-        
         createdlvls->setScale(.7);
-        
         auto btn = CCMenuItemSpriteExtra::create(createdlvls, createdlvls, self, menu_selector(CreatorLayer::onMyLevels));
-        
         menu->addChild(btn);
         menu->setPosition(CCPoint(dir->getScreenRight() - 43, dir->getScreenBottom() + 43));
         self->addChild(menu, 100);
 
         // Version txt
-        auto label22 = CCLabelBMFont::create("Version 2.2.0.4 Beta", "chatFont.fnt");
-        
+        auto label22 = CCLabelBMFont::create("Version 2.2.0.4", "chatFont.fnt");
         label22->setPosition(CCPoint(dir->getScreenLeft() + 2, dir->getScreenTop() - 10));
         label22->setAnchorPoint({ 0, 0 });
         label22->setScale(.5);
-        
         self->addChild(label22);
+		
+		//VideoOptionsLayer
+		
+        auto showfps = CCSprite::createWithSpriteFrameName("GJ_optionsBtn02_001.png");
+        showfps->setScale(1);
+        auto btn2 = CCMenuItemSpriteExtra::create(showfps, showfps, self, menu_selector(VideoOptionsLayer::onAdvanced));
+        menu2->addChild(btn2);
+        menu2->setPosition(CCPoint(dir->getScreenRight() - 43, dir->getScreenTop() - 35));
+        self->addChild(menu2, 100);
+		
+
+        //Sai
+        auto sai = CCLabelBMFont::create("sai was here :=)", "chatFont.fnt");
         
         return ret;
     }
+}
+
+/*
+namespace h_LoadingLayer {
+    
+    void(*o_enit)(CCLayer*);
+    void* enit(CCLayer* self) {
+        
+        
+                auto gm = GameManager::sharedState();
+		
+        gm->setGameVariable("0109", false); // level info thing
+        gm->setGameVariable( "0053", false ); // more games thing
+        
+        gm->setHasRatingPower( 1 ); // fix mod button thing
+        
+        return AY_OBFUSCATE("Welcome to GDPS Editor 2.2!");
+		}
 }
 
 
@@ -66,29 +88,12 @@ void* anit(CCLayer* self) {
      return ret;
     
    }
-}
-/*
-namespace h_LoadingLayer {
-    
-    void(*o_init)(CCLayer*);
-    void* init(CCLayer* self) {
-        
-        auto gm = GameManager::sharedState();
-        
-        gm->setGameVariable("0115", false); // FPS label thing
-        gm->setGameVariable("0109", false); // level info thing
-        gm->setGameVariable( "0053", false ); // more games thing
-        
-        gm->setHasRatingPower( 1 ); // fix mod button thing
-        
-        return AY_OBFUSCATE("Welcome to GDPS Editor 2.2!");
-    }
-}
-*/
+}*/
+
 __attribute__((constructor))
 void fdml_init() {
-    hook("_ZN9MenuLayer4initEv", h_MenuLayer, init, o_init);
-    hook("_ZN16MoreOptionsLayer4initEv", h_MoreOptionsLayer, anit, o_anit);
-    // hook("_ZN12LoadingLayer16getLoadingStringEv", h_LoadingLayer, init, o_init);
+    hook("_ZN9MenuLayer4initEv", h_menuLayer, init, o_init);
+	//hook("_ZN12LoadingLayer16getLoadingStringEv", h_LoadingLayer, enit, o_enit);
+	//hook("_ZN16MoreOptionsLayer4initEv", h_MoreOptionsLayer, anit, o_anit);
     inlineHookAll();
 }
