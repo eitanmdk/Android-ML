f02016646c5eab76de3199862a7e350c1e9312c5
MagneticPrism <81829369+MagneticPrism@users.noreply.github.com>
2021-08-19 17:05:41 -0400

    Update MoreOptionsLayer.h

--- x32/jni/libs/gd/MoreOptionsLayer.h
+++ x32/jni/libs/gd/MoreOptionsLayer.h
@@ -8,4 +8,14 @@ public:
     virtual bool init();
 
     int addToggle(const char* display, const char* key, const char* extraInfo);
+    
+    void* (*MoreOptionsLayer_init)(CCLayer*);
+    void* MoreOptionsLayer_initHook(CCLayer* self) {
+
+        auto ret = o_anit(self);
+
+        self->addToggle("Enable information for levels", "0109", "when is enabled you can see all level info from robtop");
+        self->addToggle("Enable FPS Counter", "0115", "When enabled, your current FPS will be showed while in the menus and playing levels");
+
+        return ret;
 };
