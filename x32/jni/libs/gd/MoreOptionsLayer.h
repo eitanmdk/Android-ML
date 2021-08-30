f80a2c3d4be7e12158f263ae04d10897d5f8f65a
MagneticPrism <81829369+MagneticPrism@users.noreply.github.com>
2021-08-30 17:17:38 -0400

    removed my stupid hooks and placing it in main.cpp

--- x32/jni/libs/gd/MoreOptionsLayer.h
+++ x32/jni/libs/gd/MoreOptionsLayer.h
@@ -8,15 +8,4 @@ public:
     virtual bool init();
 
     int addToggle(const char* display, const char* key, const char* extraInfo);
-    
-    bool (*MoreOptionsLayer_init)(MoreOptionsLayer*);
-    bool MoreOptionsLayer_initHook(MoreOptionsLayer* self) {
-
-        if (!MoreOptionsLayer_init(self)) 
-            return false;
-
-        self->addToggle("Enable information for levels", "0109", "when is enabled you can see all level info from robtop");
-        self->addToggle("Enable FPS Counter", "0115", "When enabled, your current FPS will be showed while in the menus and playing levels");
-
-        return true;
 };
