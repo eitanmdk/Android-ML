fed6ccb945bfd64754787ac1c82dca82bbf5b2f0
Eitan <72807177+eitanmdk@users.noreply.github.com>
2021-09-18 16:32:30 -0500

    Update gd.h

--- x32/jni/libs/gd/gd.h
+++ x32/jni/libs/gd/gd.h
@@ -1,6 +1,24 @@
+//Managers
+#include "GameManager.h"
+#include "GJGameLevel.h"
+#include "GJEffectManager.h"
+#include "ColorAction.h"
+
+// Other things
+#include "FLAlertLayer.h"
+#include "ButtonSprite.h"
+#include "FLAlertLayerProtocol.h"
+#include "ObjectToolbox.h"
+#include "GameObject.h"
+
+// layers
+#include "LoadingLayer.h"
+#include "LevelInfoLayer.h"
+#include "VideoOptionsLayer.h"
+#include "MoreOptionsLayer.h"
 #include "CreatorLayer.h"
 #include "MenuLayer.h"
-#include "GameManager.h"
-#include "MoreOptionsLayer.h"
-#include "LoadingLayer.h"
-#include "VideoOptionsLayer.h"
+#include "LevelSettingsObject.h"
+#include "RewardsPage.h"
+#include "LevelFeatureLayer.h"
+#include "SetGroupIDLayer.h"
