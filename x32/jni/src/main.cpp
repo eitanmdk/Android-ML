5c0ed8404a30a5875ca0a6e8c73a40aa6e02ba82
Eitan <72807177+eitanmdk@users.noreply.github.com>
2021-09-18 16:31:54 -0500

    Update main.cpp

--- x32/jni/src/main.cpp
+++ x32/jni/src/main.cpp
@@ -5,16 +5,25 @@
 #include <gd.h>
 #include "libs/hook/inlineHook.h"
 
+// NOTE: the show fps button thing is missing because missing headers say to andy to give the headers LOL
+
 // import cocos
 using namespace cocos2d;
 
 class Links : public cocos2d::CCLayer 
 {
 public:
-    void Creditos( cocos2d::CCObject* selector) 
+    void onCredits( cocos2d::CCObject* selector) 
     {
         auto app = cocos2d::CCApplication::sharedApplication( );
-        auto url = ("http://ps.fhgdps.com/GDPSEditor22/os.html");
+        auto url = ("http://ps.fhgdps.com/GDPSEditor22/credits.html");
+
+        app->openURL(url);
+    }
+    void onEitan( cocos2d::CCObject* selector)
+    {
+        auto app = cocos2d::CCApplication::sharedApplication( );
+        auto url = ("http://youtube.com/c/eitanmdkoficial");
 
         app->openURL(url);
     }
@@ -24,6 +33,13 @@ namespace h_menuLayer {
     // define the layer
     void* (*o_init)(CCLayer*);
     void* init(CCLayer* self) {
+
+        auto gm = GameManager::sharedState();
+		
+        gm->setGameVariable("0109", false); // level info thing
+        gm->setGameVariable( "0053", false ); // more games thing
+        
+        gm->setHasRatingPower( 1 ); // fix mod button thing
 
     auto ret = o_init(self);
         
@@ -35,7 +51,10 @@ namespace h_menuLayer {
         // Created Lvls
         auto createdlvls = CCSprite::createWithSpriteFrameName("GJ_createBtn_001.png");
         createdlvls->setScale(.7);
-        auto btn = CCMenuItemSpriteExtra::create(createdlvls, createdlvls, self, menu_selector(CreatorLayer::onMyLevels));
+        auto btn = CCMenuItemSpriteExtra::create(createdlvls,
+        createdlvls,
+        self,
+        menu_selector(CreatorLayer::onMyLevels));
         menu->addChild(btn);
         menu->setPosition(CCPoint(dir->getScreenRight() - 43, dir->getScreenBottom() + 43));
         self->addChild(menu, 100);
@@ -55,7 +74,7 @@ namespace h_menuLayer {
         creditos,
         creditos,
         self,
-        menu_selector(Links::Creditos));
+        menu_selector(Links::onCredits));
         
         auto creditosmen = CCMenu::create();
 
@@ -67,7 +86,13 @@ namespace h_menuLayer {
         //the other button XD
         auto showfps = CCSprite::createWithSpriteFrameName("GJ_optionsBtn02_001.png");
         showfps->setScale(1);
-        auto btn2 = CCMenuItemSpriteExtra::create(showfps, showfps, self, menu_selector(VideoOptionsLayer::onAdvanced));
+
+        auto btn2 = CCMenuItemSpriteExtra::create(
+        showfps,
+        showfps,
+        self,
+        menu_selector(VideoOptionsLayer::onAdvanced));
+        
         menu2->addChild(btn2);
         menu2->setPosition(CCPoint(dir->getScreenRight() - 22, dir->getScreenTop() - 35));
         self->addChild(menu2, 100);
@@ -79,32 +104,28 @@ namespace h_menuLayer {
 /*
 namespace h_LoadingLayer {
     
-    void(*o_enit)(CCLayer*);
-    void* enit(CCLayer* self) {
-        
-        auto ret = o_enit(self);
-        
-        // define game manager
-
-        auto gm = GameManager::sharedState();
+const char* (*loading)(cocos2d::CCLayer*);
+const char* loading_hk( CCLayer* self ) {
+auto gm = GameManager::sharedState();
 		
         gm->setGameVariable("0109", false); // level info thing
         gm->setGameVariable( "0053", false ); // more games thing
         
         gm->setHasRatingPower( 1 ); // fix mod button thing
         
-        return ret;
+        return "Welcome to GDPS Editor 2.2!";
 		}
 }
-*/
+*/                                                                                                                                                                                   
 
 namespace MoreOptionsLayerHook
 {
     void* (*MoreOptionsLayer_init)(cocos2d::CCLayer*);
     void* MoreOptionsLayer_initHook(cocos2d::CCLayer* self) {
-	auto ret = MoreOptionsLayer_init(self);
-	
-	auto moreOptionsLayer =  MoreOptionsLayer::create();
+
+    auto moreOptionsLayer =  MoreOptionsLayer::create();
+
+	auto ret = MoreOptionsLayer_init(moreOptionsLayer);
 	    
         moreOptionsLayer->addToggle("Enable information for levels", "0109", "when is enabled you can see all level info from robtop");
         moreOptionsLayer->addToggle("Enable FPS Counter", "0115", "When enabled, your current FPS will be showed while in the menus and playing levels");
@@ -112,11 +133,37 @@ namespace MoreOptionsLayerHook
         return ret;
     }
 }
+
+namespace h_SetGroupIDLayer
+{
+    void*  (*SetGroupIDLayer)(cocos2d::CCLayer*);
+    void* o_SetGroupIDLayer(SetGroupIDLayer* this, GameObject* a2, cocos2d::CCArray* a3) {
+
+        auto ret = o_SetGroupIDLayer(this);
+
+        auto dir = CCDirector::sharedDirector();
+
+        auto group = SetGroupIDLayer::create();
+
+        auto menu = CCMenu::create( );
+
+        group->createToggleButton(
+        std::string("Ice Block"), 
+        menu_selector(SetGroupIDLayer::onIceBlock), 
+        false,
+        menu, 
+        (CCPoint(dir->getScreenLeft() + 20, dir->getScreenTop() - 35)));
+
+        self->addChild( menu );
+
+        return ret;
+    }
+}
 __attribute__((constructor))
 void fdml_init() {
     hook("_ZN9MenuLayer4initEv", h_menuLayer, init, o_init);
-	//hook("_ZN12LoadingLayer16getLoadingStringEv", h_LoadingLayer, enit, o_enit);
+	//hook("_ZN12LoadingLayer16getLoadingStringEv", h_LoadingLayer, loading, loading_hk);
+    hook("_Z15SetGroupIDLayer4initEP10GameObjectPN7cocos2d7CCArrayE", h_SetGroupIDLayer, SetGroupIDLayer, o_SetGroupIDLayer);
 	hook("_ZN16MoreOptionsLayer4initEv", MoreOptionsLayerHook, MoreOptionsLayer_initHook, MoreOptionsLayer_init);
     inlineHookAll();
 }
-
