c207ceea6e972bafabdc1c04915faa838757a5e1
Eitan <72807177+eitanmdk@users.noreply.github.com>
2021-10-13 18:46:34 -0500

    Update main.cpp

--- x32/jni/src/main.cpp
+++ x32/jni/src/main.cpp
@@ -26,6 +26,17 @@ public:
         auto url = ("http://youtube.com/c/eitanmdkoficial");
 
         app->openURL(url);
+     }
+     /*
+     void onRewards(cocos2d::CCObject* selector)
+     {
+      auto layer = RewardsPage::create();
+      layer->show();
+     }
+     */
+    void onTest(cocos2d::CCObject* selector) {
+
+    FLAlertLayer::create( nullptr, "Stack Trace", "ss.str()", "Exit", nullptr, 450., true, 300. )->show( );
     }
 };
 namespace h_menuLayer {
@@ -65,23 +76,7 @@ namespace h_menuLayer {
         label22->setAnchorPoint({ 0, 0 });
         label22->setScale(.5);
         self->addChild(label22);
-
-
         // credits button **new addon**
-        auto creditos = CCSprite::createWithSpriteFrameName("communityCreditsBtn_001.png");
-
-        auto creditosbut = CCMenuItemSpriteExtra::create(
-        creditos,
-        creditos,
-        self,
-        menu_selector(Links::onCredits));
-        
-        auto creditosmen = CCMenu::create();
-
-        creditosmen->setPosition(CCPoint(dir->getScreenLeft() + 20, dir->getScreenTop() - 35));
-        creditosmen->addChild(creditosbut);
-        creditosmen->setScale(1);
-        self->addChild(creditosmen, 100);
 
         //the other button XD
         auto showfps = CCSprite::createWithSpriteFrameName("GJ_optionsBtn02_001.png");
@@ -97,15 +92,17 @@ namespace h_menuLayer {
         menu2->setPosition(CCPoint(dir->getScreenRight() - 22, dir->getScreenTop() - 35));
         self->addChild(menu2, 100);
 
+
+
         return ret;
     }
 }
 
-/*
+
 namespace h_LoadingLayer {
     
 const char* (*loading)(cocos2d::CCLayer*);
-const char* loading_hk( CCLayer* self ) {
+const char* loading_hk( CCLayer* self, void* ) {
 auto gm = GameManager::sharedState();
 		
         gm->setGameVariable("0109", false); // level info thing
@@ -113,41 +110,26 @@ auto gm = GameManager::sharedState();
         
         gm->setHasRatingPower( 1 ); // fix mod button thing
         
-        return "Welcome to GDPS Editor 2.2!";
+        return "Welcome to GDPS Editor /*
+2.2!";
 		}
-}
-*/                                                                                                                                                                                   
+}    
 
-namespace MoreOptionsLayerHook
-{
-    MoreOptionsLayer* (*CCLayer)(MoreOptionsLayer*);
-    MoreOptionsLayer* MoreOptionsLayer_initHook(CCLayer* self) {
 
-    auto ret = MoreOptionsLayer_init(self);
-        
-     
-        
-        MoreOptionsLayer::addToggle(bool, "Enable information for levels", "0109", "when is enabled you can see all level info from robtop");
-        MoreOptionsLayer::(bool, "Enable FPS Counter", "0115", "When enabled, your current FPS will be showed while in the menus and playing levels");
-
-        return ret;
-    }
-}
-
+*/
+/*
 namespace h_SetGroupIDLayer
 {
-    void*  (*SetGroupIDLayer)(cocos2d::CCLayer*);
-    void* o_SetGroupIDLayer(SetGroupIDLayer* this, GameObject* a2, cocos2d::CCArray* a3) {
+    SetGroupIDLayer*  (*SetGroupIDLayer)(cocos2d::CCLayer*);
+    SetGroupIDLayer* o_SetGroupIDLayer(cocos2d::CCLayer* self, GameObject* a2, cocos2d::CCArray* a3) {
 
-        auto ret = o_SetGroupIDLayer(this);
+        auto ret = SetGroupIDLayer(self);
 
         auto dir = CCDirector::sharedDirector();
 
-        auto group = SetGroupIDLayer::create();
-
         auto menu = CCMenu::create( );
 
-        group->createToggleButton(
+        ret->createToggleButton(
         std::string("Ice Block"), 
         menu_selector(SetGroupIDLayer::onIceBlock), 
         false,
@@ -159,11 +141,106 @@ namespace h_SetGroupIDLayer
         return ret;
     }
 }
+*/
+namespace h_GJGarageLayer
+{
+    void* (*o_GJGarageLayer)(cocos2d::CCLayer*);
+    void* GJGarageLayer(cocos2d::CCLayer* self) {
+
+        auto ret = o_GJGarageLayer(self);
+
+        auto dir = CCDirector::sharedDirector();
+
+        auto info = CCSprite::createWithSpriteFrameName("GJ_infoIcon_001.png");
+
+        auto infobut = CCMenuItemSpriteExtra::create(
+        info,
+        info,
+        self,
+        menu_selector(GJGarageLayer::onInfo));
+        
+        auto infomen = CCMenu::create();
+
+        infomen->setPosition({40, 170});
+        infomen->addChild(infobut);
+        infomen->setScale(1);
+        self->addChild(infomen);
+
+
+        return ret;
+    }
+}
+
+namespace MoreOptionsLayerHook
+{
+    MoreOptionsLayer* (*CCLayer)(MoreOptionsLayer*);
+    MoreOptionsLayer* MoreOptionsLayer_initHook(CCLayer* self) {
+
+    auto ret = MoreOptionsLayer_init(self);
+        
+        MoreOptionsLayer::addToggle(false, "Enable information for levels", "0109", "when is enabled you can see all level info from robtop");
+        MoreOptionsLayer::(false, "Enable FPS Counter", "0115", "When enabled, your current FPS will be showed while in the menus and playing levels");
+
+        return ret;
+    }
+}
+
+namespace h_CreatorLayer
+{
+    void* (*o_CreatorLayer)(cocos2d::CCLayer*);
+    void* CreatorLayer(cocos2d::CCLayer* self) {
+         auto ret = o_CreatorLayer(self);
+
+        auto dir = CCDirector::sharedDirector();
+
+
+        auto creditos = CCSprite::createWithSpriteFrameName("communityCreditsBtn_001.png");
+
+        auto creditosbut = CCMenuItemSpriteExtra::create(
+        creditos,
+        creditos,
+        self,
+        menu_selector(Links::onCredits));
+        
+        auto creditosmen = CCMenu::create();
+
+        creditosmen->setPosition({30, 30});
+        creditosmen->addChild(creditosbut);
+        creditosmen->setScale(1);
+        self->addChild(creditosmen, 100);
+
+        auto children = self->getChildren();
+		CCMenu* menu = (CCMenu*)children->objectAtIndex(1);
+
+		auto menuObjs = menu->getChildren();
+
+		CCNode* mapPack = (CCNode*)menuObjs->objectAtIndex(8);
+
+		menu->removeChild(mapPack, true);
+
+		CCNode* Featured = (CCNode*)menuObjs->objectAtIndex(6);
+		CCNode* HOF = (CCNode*)menuObjs->objectAtIndex(7);
+		CCNode* Search = (CCNode*)menuObjs->objectAtIndex(8);
+
+		CCNode* Create = (CCNode*)menuObjs->objectAtIndex(0);
+		CCNode* Saved = (CCNode*)menuObjs->objectAtIndex(1);
+		CCNode* Scores = (CCNode*)menuObjs->objectAtIndex(2);
+
+		Featured->setPositionX(Create->getPositionX());
+		HOF->setPositionX(Saved->getPositionX());
+		Search->setPositionX(Scores->getPositionX());
+
+
+        return ret;
+    }
+}
 __attribute__((constructor))
 void fdml_init() {
     hook("_ZN9MenuLayer4initEv", h_menuLayer, init, o_init);
-	//hook("_ZN12LoadingLayer16getLoadingStringEv", h_LoadingLayer, loading, loading_hk);
-    hook("_Z15SetGroupIDLayer4initEP10GameObjectPN7cocos2d7CCArrayE", h_SetGroupIDLayer, SetGroupIDLayer, o_SetGroupIDLayer);
-	hook("_ZN16MoreOptionsLayer4initEv", MoreOptionsLayerHook, MoreOptionsLayer_initHook, MoreOptionsLayer_init);
+	hook("_ZN12LoadingLayer16getLoadingStringEv", h_LoadingLayer, loading, loading_hk);
+    //hook("_Z15SetGroupIDLayer4initEP10GameObjectPN7cocos2d7CCArrayE", h_SetGroupIDLayer, SetGroupIDLayer, o_SetGroupIDLayer);
+	//hook("_ZN16MoreOptionsLayer4initEv", MoreOptionsLayerHook, MoreOptionsLayer_initHook, MoreOptionsLayer_init);
+    hook("_ZN13GJGarageLayer4initEv", h_GJGarageLayer, GJGarageLayer, o_GJGarageLayer);
+    hook("_ZN12CreatorLayer4initEv", h_CreatorLayer, CreatorLayer, o_CreatorLayer);
     inlineHookAll();
 }
